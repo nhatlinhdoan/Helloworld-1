c35f095d5212bbdc782e7cc06dcf9e8d4a801e3f
DungTran <DungTran@DungTranLaptop>
2017-04-19 21:02:49 +0700

    commit 2

--- Helloworld/src/helloworld/Helloworld.java
+++ Helloworld/src/helloworld/Helloworld.java
@@ -16,6 +16,7 @@ public class Helloworld {
      */
     public static void main(String[] args) {
         System.out.println("Helloworld");
+        System.out.println("Test commit 2");
     }
     
 }
