9a7825204ee00b1404d594a133c1b41823205915
Nhat Linh Doan <mnhatlinh.doan@gmail.com>
2017-05-05 16:55:06 +0700

    Update Helloworld.java

--- Helloworld/src/helloworld/Helloworld.java
+++ Helloworld/src/helloworld/Helloworld.java
@@ -1,5 +1,4 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -20,6 +19,12 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Helloworld");
         System.out.println("Test commit 1");
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String tmp = "18/3/2017";
+        Date date = sdf.parse(tmp);
+        System.out.println(date);
+        System.out.println(date);
     }
     
 }
